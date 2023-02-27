51f1cab84779887f8908e4263b6ad7f61a7f3593
Alan Jiang <adj2424@gmail.com>
2023-02-27 17:10:32 -0500

    pogo

--- contracts/test.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.6;
-
-contract Test {
-    uint256 count = 1;
-    function test() public {
-        count++;
-    }
-}
--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,11 @@ require('@nomicfoundation/hardhat-toolbox');
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-	solidity: '0.8.17'
+	networks: {
+		localhost: {
+			url: 'http://localhost:8545',
+			chainId: 31337
+		}
+	},
+	solidity: '0.8.8'
 };
