840d7a63ea39449cca5acb7b4ec4f7edfce00ad2
Alan Jiang <adj2424@gmail.com>
2023-02-28 17:36:24 -0500

    clean agane

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,6 @@
-require('@nomicfoundation/hardhat-toolbox');
+require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-	networks: {
-		localhost: {
-			url: 'http://localhost:8545',
-			chainId: 31337
-		}
-	},
-	solidity: '0.8.8'
+  solidity: "0.8.17",
 };
