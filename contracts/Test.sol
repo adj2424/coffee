316cd7649e993467095a18161c266f709c027726
Alan Jiang <adj2424@gmail.com>
2023-03-01 17:13:19 -0500

    coffee

---
+++ contracts/Test.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+contract Test {
+    struct Message {
+        address sender;
+        string name;
+        string message;
+        uint256 timestamp;
+    }
+    // owner of the contract
+    address payable immutable deployer;
+
+    Message[] messages;
+
+    constructor() {
+        deployer = payable(msg.sender);
+    }
+
+    //  transfer the ETH to the contract when function is called must be more than 0
+    function buyCoffee(
+        string calldata name,
+        string calldata _message
+    ) public payable {
+        require(msg.value > 0 ether, "You need to send some ETH");
+        Message memory message = Message(
+            msg.sender,
+            name,
+            _message,
+            block.timestamp
+        );
+        messages.push(message);
+    }
+
+    function withdrawAll() private {
+        require(deployer == msg.sender, "You are not the owner");
+        //transfer funds from contract to deployer
+        deployer.transfer(address(this).balance);
+    }
+
+    function getMemos() public view returns (Message[] memory) {
+        return messages;
+    }
+}
